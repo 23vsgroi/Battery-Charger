9ff839c6e00132a40faec15a5758fbf474f42b60
Zano Sgroi <zano.sgroi@icloud.com>
2026-07-09 21:27:52 -0400

    Excel,Matlab,Plecs additons
    I made the plecs simulation so that the two circuits would show the absolute worst current or voltage ripple. I worked to intigrate 12V and 24V analysis together by using excel to find that absolute worst cap and indu value if only one was used in all cases. updated Matlab simulation to have 12V and implemented a graph.

---
+++ Battery Charger Project/MATLAB/Worst_case_12.m
@@ -0,0 +1,44 @@
+clear
+Io=24;
+CRm=0.30;
+VRm=0.01;
+f = 50000;
+VLL = 16:155;
+VO = 10:0.25:14.6;
+Dmax = 0.9;
+D = VO./(VLL');
+row = size(D,1);
+col = size(D,2);
+
+%Removes impossible operating points where duty cycle > Dmax
+for i = 1:row
+    for j = 1:col
+        if D(i,j) > Dmax
+            D(i,j) = 0;
+        end
+    end
+end
+
+Is = Io*D;
+Ism=max(Is,[],2);
+Ismi=Ism';
+fgen = (0.63*VLL) + 0.50;
+Vr=VLL*VRm;
+C=Ism'./(6*(Vr.*fgen));
+Cm = max(C)
+
+%graph
+figure;
+
+yyaxis left
+plot(VLL, C, 'b-', 'LineWidth', 2);
+ylabel('Capacitance (F)');
+
+yyaxis right
+plot(VLL, fgen, 'r--', 'LineWidth', 2);
+ylabel('Frequency (Hz)');   % or (kHz) if fgen is in kHz
+
+xlabel('V_{LL} (V)');
+title('Capacitance and Generated Frequency vs V_{LL} (12V)');
+legend('Capacitance', 'f_{gen}', 'Location', 'best');
+grid on;
---
+++ Battery Charger Project/MATLAB/Worst_case_24.m
@@ -0,0 +1,43 @@
+clear
+Io=24;
+CRm=0.30;
+VRm=0.01;
+f = 50000;
+VLL = 32:155;
+VO = 20:0.5:29.2;
+Dmax = 0.9;
+D = VO./(VLL');
+row = size(D,1);
+col = size(D,2);
+
+%Removes impossible operating points where duty cycle > Dmax
+for i = 1:row
+    for j = 1:col
+        if D(i,j) > Dmax
+            D(i,j) = 0;
+        end
+    end
+end
+
+Is = Io*D;
+Ism=max(Is,[],2);
+Ismi=Ism';
+fgen = (0.63*VLL) + 0.50;
+Vr=VLL*VRm;
+C=Ism'./(6*(Vr.*fgen));
+Cm = max(C)
+%graph
+figure;
+
+yyaxis left
+plot(VLL, C, 'b-', 'LineWidth', 2);
+ylabel('Capacitance (F)');
+
+yyaxis right
+plot(VLL, fgen, 'r--', 'LineWidth', 2);
+ylabel('Frequency (Hz)');   % or (kHz) if fgen is in kHz
+
+xlabel('V_{LL} (V)');
+title('Capacitance and Generated Frequency vs V_{LL} (24V)');
+legend('Capacitance', 'f_{gen}', 'Location', 'best');
+grid on;
